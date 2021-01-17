8a64e1ffc18213dbbd77155183d9f769c6dd796b
Donnie Cameron <macnod@gmail.com>
2021-01-16 19:03:44 -0800

    Package entries

--- dc-dlist-package.lisp
+++ dc-dlist-package.lisp
@@ -11,16 +11,22 @@
    dlist-node
    find-first-node
    from-list
+   head
    insert-after-node
    insert-before-node
+   len
+   next
    node-at
    peek-head 
    peek-tail 
    pop-head 
-   pop-tail 
+   pop-tail
+   prev
    push-head 
    push-tail 
    sorted
+   tail
    to-list
+   value
 
 ))
